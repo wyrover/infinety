b4e9c2e973905ccb92b4832e960326561c367293
lstables <lstables1@gmail.com>
2015-08-23 11:54:08 +0100

    meta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -167,8 +167,8 @@
 {{--</div>--}}
 <div class="section white">
     <div class="row container">
-        <h2 class="header">Parallax</h2>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+        <h4 class="header">Our Work</h4>
+        <p class="grey-text text-darken-3 lighten-3">Coming soon.</p>
     </div>
 </div>
 
