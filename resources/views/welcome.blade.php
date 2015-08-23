e1071cb2ce19e658a7d304f913bdc1a910c2dad9
lstables <lstables1@gmail.com>
2015-08-23 16:24:03 +0100

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,24 +13,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-<nav class="dark-blue" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="images/infinety.png" alt="Infinety - Web Design Sheffield" width="150" class="responsive-img"></a>
-        <ul class="right hide-on-med-and-down">
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Media</a></li>
-            <li><a href="#">Web Apps</a></li>
-        </ul>
+    <nav class="dark-blue" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="/images/infinety.png" alt="Infinety - Web Design Sheffield" width="150" class="responsive-img"></a>
+            {{--<ul class="right hide-on-med-and-down">--}}
+                {{--<li><a href="#">Web Development</a></li>--}}
+                {{--<li><a href="#">Media</a></li>--}}
+                {{--<li><a href="#">Web Apps</a></li>--}}
+            {{--</ul>--}}
 
-        <!-- Mobile Nav -->
-        <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Media</a></li>
-            <li><a href="#">Web Apps</a></li>
-        </ul>
-        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-</nav>
-
+            {{--<!-- Mobile Nav -->--}}
+            {{--<ul id="nav-mobile" class="side-nav">--}}
+                {{--<li><a href="#">Web Development</a></li>--}}
+                {{--<li><a href="#">Media</a></li>--}}
+                {{--<li><a href="#">Web Apps</a></li>--}}
+            {{--</ul>--}}
+            {{--<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>--}}
+        </div>
+    </nav>
 
     <div class="col s12">
         <div class="slider">
@@ -50,14 +49,6 @@
                         <h5 class="light grey-text text-lighten-3">Creative websites that work for you</h5>
                     </div>
                 </li>
-
-                {{--<li>--}}
-                    {{--<img src="https://images.unsplash.com/24/DSC02021.JPG?q=80&fm=jpg&s=b8ca2293b871e61f5a090f1974f58543"> <!-- random image -->--}}
-                    {{--<div class="caption center-align">--}}
-                        {{--<h3></h3>--}}
-                        {{--<h5 class="light grey-text text-lighten-3"></h5>--}}
-                    {{--</div>--}}
-                {{--</li>--}}
             </ul>
         </div>
     </div>
@@ -80,9 +71,9 @@
                 <div class="col s12 m4">
                     <div class="icon-block">
                         <h2 class="center brown-text"><i class="medium material-icons">group</i></h2>
-                        <h5 class="center">User Experience Focused</h5>
+                        <h5 class="center">User Friendly Website</h5>
 
-                        <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                        <p class="light">Focusing on the design and usability of a website is key in every sector, we use our experience and tech know how to make this happen. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
                     </div>
                 </div>
 
@@ -91,13 +82,16 @@
                         <h2 class="center brown-text"><i class="medium material-icons">settings</i></h2>
                         <h5 class="center">Easy to work with</h5>
 
-                        <p class="light">With our in-house custom built platform, updating your website has never been easier. From creating new pages, editing existing pages along with uploading images, videos or whatever you need on your website. Plus you can run a blog from the same system without having to use an alternative source.</p>
+                        <p class="light">With our in-house custom built platform, updating your website has never been easier. From creating new pages, editing existing pages along with uploading images, videos or whatever you need on your website. Plus run a blog from the same system without having to use an alternative source.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="divider"></div>
+</div>
+<div class="clients">
+    <div class="container">
     <div class="row content">
         <h4>Our Clients</h4>
         <div class="col s6">
@@ -158,20 +152,32 @@
 
         </div>
     </div>
-    <div class="divider"></div>
+ </div>
 </div>
 
-
-{{--<div class="parallax-container">--}}
-    {{--<div class="parallax"><img src="https://images.unsplash.com/24/DSC02021.JPG?q=80&fm=jpg&s=b8ca2293b871e61f5a090f1974f58543"></div>--}}
-{{--</div>--}}
 <div class="section white">
     <div class="row container">
         <h4 class="header">Our Work</h4>
-        <p class="grey-text text-darken-3 lighten-3">Coming soon.</p>
+        <div class="row">
+            <div class="col s12 m4">
+                <img src="/images/wscc.png">
+            </div>
+
+            <div class="col s12 m4">
+                <img src="/images/mittar-site.png">
+            </div>
+
+            <div class="col s12 m4">
+                <img src="/images/crmple-site.png">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <img src="/images/avoin-site.png">
+            </div>
+        </div>
     </div>
 </div>
-
 
 <footer class="page-footer dark-blue">
     <div class="container">
@@ -179,8 +185,8 @@
             <div class="col l6 s12">
                 <h5 class="white-text">Who are we?</h5>
                 <p class="grey-text text-lighten-4">
-                    We provide website development & design for everyone, from startup businesses to fully established businesses.
-                    Each website/application we create is built from scratch and to the highest of standards.
+                    We provide website development & design for everyone, from startup to fully established businesses.
+                    Each website we create is built from scratch to the highest of standards.
                 </p>
             </div>
 
@@ -196,7 +202,7 @@
         <div class="container">
             <div class="row">
                 <div class="col l8">
-                    &copy; {!! date('Y') !!} Infinety Web Ltd<br />
+                    &copy; {!! date('Y') !!} Infinety Web Ltd&nbsp;&bull;&nbsp;
                     all rights reserved
                 </div>
                 <div class="col l4 right-align">
@@ -212,11 +218,15 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
 <script>
     $(document).ready(function(){
+        // Responsive Nav
         $(".button-collapse").sideNav();
+        // Material box
         $('.materialboxed').materialbox();
+        // Slider
         $('.slider').slider({
             indicators: false
         });
+        // Parallax
         $('.parallax').parallax();
     });
 </script>
