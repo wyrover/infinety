8377bd0f26499f2f13093b8ecca357c918d2b33b
lstables <lstables1@gmail.com>
2015-10-22 21:30:13 +0100

    updates and blog

--- resources/views/blog-item.blade.php
+++ resources/views/blog-item.blade.php
@@ -11,7 +11,8 @@
     <link type="text/css" rel="stylesheet" href="/css/main.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></script>
 </head>
 <body>
 @include('partials.header')
