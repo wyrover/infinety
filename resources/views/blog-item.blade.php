45d7a14b1a49112eea0ea5e9372173805e0624f5
lstables <lstables1@gmail.com>
2015-10-22 21:25:49 +0100

    updates and blog

--- resources/views/blog-item.blade.php
+++ resources/views/blog-item.blade.php
@@ -11,6 +11,7 @@
     <link type="text/css" rel="stylesheet" href="/css/main.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
 </head>
 <body>
 @include('partials.header')
