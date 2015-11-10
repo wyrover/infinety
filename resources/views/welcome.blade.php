7fa3e880d1bc5540036a450c194e29b562a68e9e
lstables <lstables1@gmail.com>
2015-11-10 18:20:17 +0000

    ga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="/css/sweetalert.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="google-site-verification" content="Yl0MBicJD-MuoWVwuP9LRMg4A3vbzuSkx9PgH7DPmag" />
 </head>
 <body>
     @include('partials.header')
