b20ac80064d4c0557c0d7b53b6bb54e21fe0c75a
lstables <lstables1@gmail.com>
2016-01-14 22:23:37 +0000

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,300,500' rel='stylesheet' type='text/css'>
     <link type="text/css" rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/sweetalert.css">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="google-site-verification" content="Yl0MBicJD-MuoWVwuP9LRMg4A3vbzuSkx9PgH7DPmag" />
